6326f8aa9843fdb72e6796dbf0c6434fc18fbdc2
Tim <47142618+KramerTim@users.noreply.github.com>
2019-01-31 08:31:51 +0100

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.CmdRandomColor = New System.Windows.Forms.Button()
+        Me.PnlColor = New System.Windows.Forms.Panel()
+        Me.CmdPartyMode = New System.Windows.Forms.Button()
+        Me.TmrParty = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'CmdRandomColor
+        '
+        Me.CmdRandomColor.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.CmdRandomColor.Location = New System.Drawing.Point(344, 420)
+        Me.CmdRandomColor.Name = "CmdRandomColor"
+        Me.CmdRandomColor.Size = New System.Drawing.Size(185, 23)
+        Me.CmdRandomColor.TabIndex = 0
+        Me.CmdRandomColor.Text = "Random Color"
+        Me.CmdRandomColor.UseVisualStyleBackColor = True
+        '
+        'PnlColor
+        '
+        Me.PnlColor.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PnlColor.Location = New System.Drawing.Point(12, 12)
+        Me.PnlColor.Name = "PnlColor"
+        Me.PnlColor.Size = New System.Drawing.Size(848, 402)
+        Me.PnlColor.TabIndex = 1
+        '
+        'CmdPartyMode
+        '
+        Me.CmdPartyMode.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.CmdPartyMode.Location = New System.Drawing.Point(12, 420)
+        Me.CmdPartyMode.Name = "CmdPartyMode"
+        Me.CmdPartyMode.Size = New System.Drawing.Size(75, 23)
+        Me.CmdPartyMode.TabIndex = 2
+        Me.CmdPartyMode.Text = "Party Mode"
+        Me.CmdPartyMode.UseVisualStyleBackColor = True
+        '
+        'TmrParty
+        '
+        Me.TmrParty.Interval = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(872, 455)
+        Me.Controls.Add(Me.CmdPartyMode)
+        Me.Controls.Add(Me.PnlColor)
+        Me.Controls.Add(Me.CmdRandomColor)
+        Me.Name = "Form1"
+        Me.Text = "Funny Color"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents CmdRandomColor As Button
+    Friend WithEvents PnlColor As Panel
+    Friend WithEvents CmdPartyMode As Button
+    Friend WithEvents TmrParty As Timer
+End Class
---
+++ Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Randomize()
+
+    End Sub
+
+    Private Sub CmdRandomColor_Click(sender As Object, e As EventArgs) Handles CmdRandomColor.Click
+
+        Dim R, G, B As Integer
+
+        R = Math.Floor(Rnd() * 255) + 1
+        G = Math.Floor(Rnd() * 255) + 1
+        B = Math.Floor(Rnd() * 255) + 1
+
+        PnlColor.BackColor = Color.FromArgb(R, G, B)
+
+    End Sub
+
+    Private Sub CmdPartyMode_Click(sender As Object, e As EventArgs) Handles CmdPartyMode.Click
+
+        If Not TmrParty.Enabled Then
+            TmrParty.Enabled = True
+        Else
+            TmrParty.Enabled = False
+        End If
+
+    End Sub
+
+    Private Sub TmrParty_Tick(sender As Object, e As EventArgs) Handles TmrParty.Tick
+
+        CmdRandomColor_Click(sender, e)
+        'TmrParty.Interval = Math.Floor(Rnd() * 1000) + 1
+
+    End Sub
+
+End Class
